b4931ad87b46114cba8d2c5ce59f0cbf723df797
tobialfani <45823073+tobialfani@users.noreply.github.com>
2019-06-25 19:06:45 +0700

    Update js.js

--- js.js
+++ js.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 const food = document.querySelectorAll('.food');
 const ul = document.querySelector('.listFood');
 const coba = document.querySelectorAll('.food p');
+const price = document.querySelector('.price span');
 
 container.addEventListener('click',function(e){
     if(e.target.className=='close'){
@@ -9,18 +10,42 @@ container.addEventListener('click',function(e){
     }
 })
 
-food.forEach(function(e){
-    e.addEventListener('click',function(ee){
+let data = 0;
+function totalPrice(priceFoodp) {
+    let priceFood = parseInt(priceFoodp.getAttribute('data-price'));
+    data = data + priceFood;
+    price.innerHTML=data;
+}
+
+for(const a of food){
+    a.addEventListener('click',function(e){
         const liFood = document.createElement('li');
-        const nameFood = document.createTextNode('Burger');
+        const nameFood = a.getAttribute('data-name');
+        const nameFood2 = nameFood.charAt(0).toUpperCase() + nameFood.slice(1);
         const span = document.createElement('span');
         const spanName = document.createTextNode('x');
         span.appendChild(spanName);
         span.setAttribute('class','close');
-        liFood.appendChild(nameFood);
+        liFood.append(nameFood2);
         liFood.appendChild(span);
         ul.appendChild(liFood);
-        //untuk menghentikan fungsi default tag a
-        ee.preventDefault();
+        totalPrice(a);
     })
-})
+}
+
+
+// food.forEach(function(e){
+//     e.addEventListener('click',function(ee){
+//         const liFood = document.createElement('li');
+//         const nameFood = document.createTextNode('Burger');
+//         const span = document.createElement('span');
+//         const spanName = document.createTextNode('x');
+//         span.appendChild(spanName);
+//         span.setAttribute('class','close');
+//         liFood.appendChild(nameFood);
+//         liFood.appendChild(span);
+//         ul.appendChild(liFood);
+//         //untuk menghentikan fungsi default tag a
+//         ee.preventDefault();
+//     })
+// })
