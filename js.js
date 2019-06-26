49b17a89ca6294b67b61d4ba0572f651ab724e2b
tobialfani <45823073+tobialfani@users.noreply.github.com>
2019-06-26 20:29:52 +0700

    Update js.js

--- js.js
+++ js.js
@@ -4,32 +4,47 @@ const ul = document.querySelector('.listFood');
 const coba = document.querySelectorAll('.food p');
 const price = document.querySelector('.price span');
 
+//func close
 container.addEventListener('click',function(e){
     if(e.target.className=='close'){
-        e.target.parentElement.remove();
+        let foodTarget = e.target.parentElement;
+        totalPrice(foodTarget,"min");
+        foodTarget.remove();
     }
 })
 
+//func Harga total
 let data = 0;
-function totalPrice(priceFoodp) {
+function totalPrice(priceFoodp,type) {
     let priceFood = parseInt(priceFoodp.getAttribute('data-price'));
-    data = data + priceFood;
+    if(type=="plus"){
+        data = data + priceFood;
+    }else if(type=="min"){
+        data = data - priceFood;
+    }
     price.innerHTML=data;
 }
+//func pembentukan li
+function makeLi(makeLiP){
+    let priceFood = parseInt(makeLiP.getAttribute('data-price'));
+    const liFood = document.createElement('li');
+    const nameFood = makeLiP.getAttribute('data-name');
+    const nameFood2 = nameFood.charAt(0).toUpperCase() + nameFood.slice(1);
+    const span = document.createElement('span');
+    const spanName = document.createTextNode('x');
+    span.appendChild(spanName);
+    span.setAttribute('class','close');
+    liFood.setAttribute('data-price',priceFood);
+    liFood.append(nameFood2);
+    liFood.appendChild(span);
+    ul.appendChild(liFood);
+}
 
-for(const a of food){
-    a.addEventListener('click',function(e){
-        const liFood = document.createElement('li');
-        const nameFood = a.getAttribute('data-name');
-        const nameFood2 = nameFood.charAt(0).toUpperCase() + nameFood.slice(1);
-        const span = document.createElement('span');
-        const spanName = document.createTextNode('x');
-        span.appendChild(spanName);
-        span.setAttribute('class','close');
-        liFood.append(nameFood2);
-        liFood.appendChild(span);
-        ul.appendChild(liFood);
-        totalPrice(a);
+//ketika makanan diklik
+for(const foodTarget of food){
+    foodTarget.addEventListener('click',function(){
+        makeLi(foodTarget);
+        totalPrice(foodTarget,"plus");
     })
 }
 
